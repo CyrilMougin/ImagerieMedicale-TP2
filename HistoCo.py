e1b2653a38f0fe3fef1dd6376e8c808349ba3688
CyrilMougin <38719033+CyrilMougin@users.noreply.github.com>
2019-10-27 17:37:23 -0400

    Initial commit

---
+++ HistoCo.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 19:24:09 2019
+
+@author: cmoug
+"""
+
+
+import numpy as np
+import cv2 
+import matplotlib.pyplot as plt
+
+np.set_printoptions(suppress=True)
+  
+# Save image in set directory 
+# Read RGB image 
+img1 = plt.imread('D:\DataTP2\Data\I4.jpg')  
+img2 = plt.imread('D:\DataTP2\Data\J4.jpg')
+  
+
+
+
+#plt.imshow(img1,cmap=plt.get_cmap('gray'))
+
+I = np.unique(img1).shape[0]
+J = np.unique(img2).shape[0]
+
+
+
+
+
+
+    
+def JointHist(I,  J,  bin) :
+    Conjoint =  np.zeros((I,J))
+    for i in range (img1.shape[0]) :
+        for j in range (img1.shape[1]):
+            Conjoint[img1[i,j],img2[i,j]]+=1 
+            if  (Conjoint[img1[i,j],img2[i,j]]>255):
+                Conjoint[img1[i,j],img2[i,j]]-=1
+        
+    
+    return Conjoint
+    
+
+plt.imshow(JointHist(I,J,bin), vmin=0, vmax=255)
+
+cv2.imshow('image',JointHist(I,J,bin))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+  
