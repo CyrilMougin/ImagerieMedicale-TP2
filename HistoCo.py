8105a2403f222c32573ea1cb48d035fb148b912c
CyrilMougin <c.mougin@hotmail.fr>
2019-10-29 21:48:56 -0400

    Update HistoCo.py

--- HistoCo.py
+++ HistoCo.py
@@ -7,47 +7,41 @@ Created on Sat Oct 26 19:24:09 2019
 
 
 import numpy as np
-import cv2 
 import matplotlib.pyplot as plt
+from matplotlib import pyplot
+
+
 
 np.set_printoptions(suppress=True)
+
+
+
+
   
 # Save image in set directory 
 # Read RGB image 
-img1 = plt.imread('D:\DataTP2\Data\I4.jpg')  
-img2 = plt.imread('D:\DataTP2\Data\J4.jpg')
+img1 = plt.imread('D:\Sherbrooke\ImagerieMedicale-TP2\DataTP2\Data\I2.jpg')  
+img2 = plt.imread('D:\Sherbrooke\ImagerieMedicale-TP2\DataTP2\Data\J2.jpg')
   
-
-
-
-#plt.imshow(img1,cmap=plt.get_cmap('gray'))
-
-I = np.unique(img1).shape[0]
-J = np.unique(img2).shape[0]
-
-
-
-
-
 
     
 def JointHist(I,  J,  bin) :
-    Conjoint =  np.zeros((I,J))
-    for i in range (img1.shape[0]) :
-        for j in range (img1.shape[1]):
-            Conjoint[img1[i,j],img2[i,j]]+=1 
-            if  (Conjoint[img1[i,j],img2[i,j]]>255):
-                Conjoint[img1[i,j],img2[i,j]]-=1
-        
+    Conjoint =  np.zeros((int(np.amax(I))+1,(int(np.amax(J))+1)))
+    for i in range (I.shape[0]) :
+        for j in range (I.shape[1]):
+            Conjoint[I[i,j],J[i,j]]+=1 
+            if  (Conjoint[I[i,j],J[i,j]]>255):
+                Conjoint[I[i,j],J[i,j]]-=1
+
     
-    return Conjoint
+    
+    plt.imshow(Conjoint, origin='lower',cmap = 'jet')
+    
+    
     
 
-plt.imshow(JointHist(I,J,bin), vmin=0, vmax=255)
 
-cv2.imshow('image',JointHist(I,J,bin))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+JointHist(img1,img2,bin)
 
 
   
