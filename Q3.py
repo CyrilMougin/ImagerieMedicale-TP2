adbe0b3a46d945b12f108e3c6ab4ad37d22934bc
CyrilMougin <c.mougin@hotmail.fr>
2019-11-01 21:27:15 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -5,41 +5,32 @@ Created on Tue Oct 29 22:47:46 2019
 @author: cmoug
 """
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import proj3d 
-import math
+
+
+
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
 N = 2
-x,y,z = np.meshgrid(np.arange(0, 4, 1), np.arange(0, 4, 1),np.arange(0, 5, 1))
-print(x)
-
+x,y,z = np.mgrid[0:20:2, 0:20:2, 0:20:1]
+print(x )
+print(y)
 #T = np.exp(-x**2 - y**2 - z**2)
 
 fig = plt.figure()
-
 ax = fig.add_subplot(111, projection='3d')
 scat = ax.scatter(x, y, z, alpha=0.5)
 
-
 plt.tight_layout()
 
-
-
-
-
-
 def trans_rigide(theta, omega, phi, p, q, r) :
-    
-    R_x = np.array([[1,         0,                  0                   ],
-                    [0,         math.cos(0.5), -math.sin(0.5) ],
-                    [0,         math.sin(0.5), math.cos(0.5)  ]
-                    ])
-    
-    
-    return ax.scatter(R_x*x,y,z, alpha = 1)
-        
-trans_rigide(0,0,0,0,0,0)
+    trans = np.array([[p,0,0],[0,q,0],[0,0,r]])
+    rx = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]])
+    ry = np.array([[np.cos(omega),0,np.sin(omega)],[0,1,0],[-np.sin(omega),0,np.cos(omega)]])
+    rz = np.array([[np.cos(phi),-np.sin(phi),0],[np.sin(phi),np.cos(phi),0],[0,0,1]])
+    return trans*rx*ry*rz
