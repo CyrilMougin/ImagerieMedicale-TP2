c53bc2efbd04ea93e3b0f2c36913319221c0c97c
Julien <jbrochier@ensc.fr>
2019-11-03 18:25:25 -0500

    merge

--- Q3.py
+++ Q3.py
@@ -24,11 +24,13 @@ scat = ax.scatter(x, y, z, alpha=0.5)
 
 plt.tight_layout()
 
-def trans_rigide(theta, omega, phi, p, q, r) :
+def trans_rigide(theta, omega, phi, p, q, r):
     trans = np.array([[p,0,0],[0,q,0],[0,0,r]])
     rx = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]])
     ry = np.array([[np.cos(omega),0,np.sin(omega)],[0,1,0],[-np.sin(omega),0,np.cos(omega)]])
     rz = np.array([[np.cos(phi),-np.sin(phi),0],[np.sin(phi),np.cos(phi),0],[0,0,1]])
     return trans*rx*ry*rz
+    
+def similitude(s,theta, omega, phi, p, q, r):
     
         
