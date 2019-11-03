bc387b0617dc532b03b40142fdeb5753d52a291d
CyrilMougin <c.mougin@hotmail.fr>
2019-11-02 22:33:51 -0400

    similtude

--- Q3.py
+++ Q3.py
@@ -29,7 +29,7 @@ print(tot[0])
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-scat = ax.scatter(x, y, z, alpha=0.9)
+scat = ax.scatter(x, y, z, alpha=0.1)
 
 
 plt.tight_layout()
@@ -46,8 +46,19 @@ def trans_rigide(theta, omega, phi, p, q, r) :
 
     return trans.dot(rx).dot(ry).dot(rz)
 
-print (trans_rigide(0,0,0,0,0,0) )
-tot = (trans_rigide(10,0,0,10,10,10) ).dot(tot)
+def similitude(s,theta, omega, phi, p, q, r) :
+    simi = np.array([[s,0,0,0],[0,s,0,0],[0,0,s,0],[0,0,0,1]])
+    trans = np.array([[1,0,0,p],[0,1,0,q],[0,0,1,r],[0,0,0,1]])
+    rx = np.array([[1,0,0,0],[0,np.cos(theta),-np.sin(theta),0],[0,np.sin(theta),np.cos(theta),0],[0,0,0,1]])
+    ry = np.array([[np.cos(omega),0,-np.sin(omega),0],[0,1,0,0],[np.sin(omega),0,np.cos(omega),0],[0,0,0,1]])
+    rz = np.array([[np.cos(phi),-np.sin(phi),0,0],[np.sin(phi),np.cos(phi),0,0],[0,0,1,0],[0,0,0,1]])
+
+    return simi.dot(trans.dot(rx).dot(ry).dot(rz))
+
+
+
+
+tot = (similitude(1,0,0,0,0,0,40) ).dot(tot)
 
 scat1 = ax.scatter(tot[0], tot[1], tot[2], alpha=0.5)
 
