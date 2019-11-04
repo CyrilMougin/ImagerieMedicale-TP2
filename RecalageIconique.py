2db39c00bdf4ca2b81e68740e2f64253a0414781
SalomeGomezP <36883497+SalomeGomezP@users.noreply.github.com>
2019-11-03 21:48:50 -0500

    Update RecalageIconique.py

--- RecalageIconique.py
+++ RecalageIconique.py
@@ -66,24 +66,25 @@ def translation(I, p, q):
 # =============================================================================
 # Q5.b
 # =============================================================================
-def recalage2DLucasKanade(I1,I2) : #p95
-    Ix = np.gradient(I2)[0]
-    Iy = np.gradient(I2)[1]
-    It = I2-I1
+def recalage2DLucasKanade(I,J) : #p95
+    gradient=np.gradient(J)
+    Jx = gradient[0]
+    Jy = gradient[1]
+    Jt = J-I
     
-    M=[[np.sum(Ix*Ix), np.sum(Iy*Ix)],[np.sum(Iy*Ix), np.sum(Iy*Iy)]]
-    b=[[np.sum(Ix*It)], [np.sum(Iy*It)]]
+    M=np.array([[np.sum(Jx*Jx), np.sum(Jy*Jx)],[np.sum(Jy*Jx), np.sum(Jy*Jy)]])
+    b=np.array([[np.sum(Jx*Jt)], [np.sum(Jy*Jt)]])
     Minv=np.linalg.inv(M)  
 
-    u=np.dot(-Minv,b)
-   # print(u)
-    translat=translation(I2,u[0], u[1])
+    u=-np.dot(Minv,b)
+    print(u)
+    translat=translation(J,u[0], u[1])
    
 
     return translat
 
-
-recalage=recalage2DLucasKanade(BrainMRI_1,BrainMRI_4)
+tmp=translation(I2, -80.8,-100)
+recalage=recalage2DLucasKanade(I2,tmp)
 plt.imshow(recalage,cmap='gray')
 
 
