bf189b60da2f912c9ae52880f1810bfbd2a1df46
CyrilMougin <c.mougin@hotmail.fr>
2019-11-08 22:31:49 -0500

    Update HistoCo.py

--- HistoCo.py
+++ HistoCo.py
@@ -12,18 +12,26 @@ from matplotlib import pyplot
 
 
 
-np.set_printoptions(suppress=True)
 
 
 
 
+
+#I1 = np.int(plt.imread('DataTP2\Data\I1.png')[:,:,1]  ) * 255
+#J1 = np.int(plt.imread('DataTP2\Data\J1.png')[:,:,1]  ) * 255
+I2 = plt.imread('DataTP2\Data\I2.jpg')
+J2 = plt.imread('DataTP2\Data\J2.jpg')
+I3 = plt.imread('DataTP2\Data\I3.jpg')
+J3 = plt.imread('DataTP2\Data\J3.jpg')
+I4 = plt.imread('DataTP2\Data\I4.jpg')  
+J4 = plt.imread('DataTP2\Data\J4.jpg')
+I5 = plt.imread('DataTP2\Data\I5.jpg')  
+J5 = plt.imread('DataTP2\Data\J5.jpg')
+I6 = plt.imread('DataTP2\Data\I6.jpg') 
+J6 = plt.imread('DataTP2\Data\J6.jpg')
   
-# Save image in set directory 
-# Read RGB image 
-img1 = plt.imread('DataTP2\Data\I2.jpg')  
-img2 = plt.imread('DataTP2\Data\J2.jpg')
-  
 
+    
     
 def JointHist(I,  J,  Bin) :
     Conjoint =  np.zeros((int(np.amax(I))+1,(int(np.amax(J))+1)))
@@ -35,11 +43,18 @@ def JointHist(I,  J,  Bin) :
             yMin=J[i,j]//intervalle
             Conjoint[ xMin*intervalle:xMin*intervalle +intervalle,yMin*intervalle:yMin*intervalle+intervalle]+=1 
             
-            
-#            if  (Conjoint[I[i,j],J[i,j]]>255):
-#                Conjoint[I[i,j],J[i,j]]-=1
 
+    Conjoint = np.asarray(Conjoint)
+    Conjoint = np.log(Conjoint)
     
+   
+    
+    sum= 0
+    
+    for i in range (Conjoint.shape[0]) :
+        for j in range (Conjoint.shape[1]):
+            sum = sum + Conjoint[i,j]
+    print(sum)
 
     return  Conjoint
     
@@ -47,11 +62,10 @@ def JointHist(I,  J,  Bin) :
     
 
 
-histo=JointHist(img1,img2,2)
-plt.imshow(histo, origin='lower',cmap = 'jet')
+histo=JointHist(I6,J6,100)
+ig, ax = plt.subplots()
+ax.imshow(histo, origin='lower',cmap = 'jet')
 
 
-a=np.array([[1,1,1,1],[1,1,1,1]])
-a[0:1,1:3]=0
-
+plt.show()
   
