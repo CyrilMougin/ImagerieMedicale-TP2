d24eaf14001782af90dfda02dd7c10811e537255
SalomeGomezP <36883497+SalomeGomezP@users.noreply.github.com>
2019-11-11 01:04:17 -0500

    fin

--- HistoCo.py
+++ HistoCo.py
@@ -64,19 +64,10 @@ def JointHist(I,  J,  Bin) :
 
 
 
-histo=JointHist(I2,J2,160)
-ig, ax = plt.subplots()
-ax.imshow(histo, origin='lower',cmap = 'jet')
-
-plt.show()
-  
-
-# =============================================================================
-# histo=JointHist(I6,J6,100)
-# ig, ax = plt.subplots()
-# ax.imshow(histo, origin='lower',cmap = 'jet')
-# 
-# plt.show()
-#   
-# =============================================================================
+#histo=JointHist(I2,J2,160)
+#ig, ax = plt.subplots()
+#ax.imshow(histo, origin='lower',cmap = 'jet')
+#
+#plt.show()
+#  
 
